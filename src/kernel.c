0fad545189349694dbfbd547ccc6cabea5546097
reend <reend@example.com>
2026-09-22 22:09:54 +0200

    io

--- src/kernel.c
+++ src/kernel.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "idt/idt.h"
+#include "io/io.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -75,4 +76,6 @@ void kernel_main()
     print("Hello World\ntest");
 
     idt_init();
+
+    outb(0x60, 0xff);
 }
